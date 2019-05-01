7e2afc71f94e3072eba5e3b99dba566504bfa613
unknown <miameroi@yahoo.com>
2019-05-01 16:18:06 +0100

    edited cards array

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,54 +1,88 @@
 console.log("Up and running!");
-
+//Array of cards options
 var cards = [
 {
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
+	word: "dog",
+	language: "english",
+	cardImage: "images/dog-en.jpg"
 },
 {
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
+	word: "cat",
+	language: "english",
+	cardImage: "images//cat-en.jpg"
 },
 {
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
+	word: "dog",
+	language: "italian",
+	cardImage: "images//dog-it.jpg"
 },
 {
-	rank: "king",
-	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
+	word: "milk",
+	language: "english",
+	cardImage: "images/milk-en.jpg"
+},
+{
+	word: "cat",
+	language: "italian",
+	cardImage: "images//cat-it.jpg"
+},
+{
+	word: "milk",
+	language: "italian",
+	cardImage: "images//milk-it.jpg"
+},
+{
+	word: "bread",
+	language: "italian",
+	cardImage: "images//bread-it.jpg"
+},
+{
+	word: "bread",
+	language: "english",
+	cardImage: "images//bread-en.jpg"
 },
 ];
+//Array of the cards picked by the player
 var cardsInPlay = [];
 
+//function checks if the cards picked are the same 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  	console.log("You found a match!");
+  	alert("Congratulations, You found a match! Refresh the page to play again");
 	} 
 	else {
-  	alert("Sorry, try again.");
+  	alert("Sorry, not a match! Refresh the page to play again");
 	}
 }
 
-function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
+//
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	
+	console.log("User flipped " + cards[cardId].word);
+	cardsInPlay.push(cards[cardId].word);
 	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	console.log(cards[cardId].language);
+//matches the images with the src
+	this.setAttribute('src', cards[cardId].cardImage);
+
+//check if two cards were played and runs the alert function
 	if (cardsInPlay.length === 2) {
 	checkForMatch();
 	}
-	else {
-	return(cards[cardId]);
+}
+// create a new game
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src',"images/back.jpg");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
 	}
-	
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
 
 
 
